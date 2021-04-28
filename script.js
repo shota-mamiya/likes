8f3727bdcc61206395f53310ea9e00ffe18dd829
shota-mamiya <shota.mamiya@outlook.com>
2021-04-27 17:24:47 -0700

    Likes

---
+++ script.js
@@ -0,0 +1,20 @@
+var count = 0;
+var countElement = document.querySelector("#count");
+function addLike(element){
+    count++;
+    countElement.innerText = count + " Like(s)";
+}
+var count1 = 0;
+var count1Element = document.querySelector("#count1");
+function addLike1(element){
+    count1++;
+    count1Element.innerText = count1 + " Like(s)";
+
+}
+var count2 = 0;
+var count2Element = document.querySelector("#count2");
+function addLike2(element){
+    count2++;
+    count2Element.innerText = count2 + " Like(s)";
+
+}
